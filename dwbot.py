56a1458e1a2dc90be69abb0cf84eb117f7be4fda
free-up <frago@mail.ru>
2022-03-10 05:12:49 +0300

    Add files via upload

--- dwbot.py
+++ dwbot.py
@@ -8,7 +8,7 @@ config_dict = get_config_from('defo.json')
 owm = OWM('8e73d515dcfe39452a17e88b4fc00e51', config_dict)
 mgr = owm.weather_manager()
 
-import favicon from serve-favicon
+import favicon
 server.use(favicon(path.join(__dirname, "../assets/images/favicon.ico")))
 
 
