c29c52d3e67af2c006bfa1ed15fbffe64d842733
free-up <frago@mail.ru>
2022-03-11 01:35:59 +0300

    Add files via upload

--- dwbot.py
+++ dwbot.py
@@ -8,9 +8,6 @@ config_dict = get_config_from('defo.json')
 owm = OWM('8e73d515dcfe39452a17e88b4fc00e51', config_dict)
 mgr = owm.weather_manager()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
 
 
 
